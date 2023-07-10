f8892de2243eedf351cab4aa0f76eb293257063a
ANANDALAKSHMI <117814711+anandalakshmis@users.noreply.github.com>
2023-07-10 15:32:28 +0530

    Add files via upload

---
+++ ananda/mul.java
@@ -0,0 +1,11 @@
+package CO4Q5;
+
+public class mul extends Thread{
+    public void run(){
+        System.out.println("\n");
+        for(int i =0;i<11;i++){
+            System.out.println("5*"+i+" = "+5*i);
+        }
+    }
+public static void main(String[] args){} 
+}
